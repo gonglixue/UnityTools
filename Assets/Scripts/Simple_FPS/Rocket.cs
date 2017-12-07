f745641aa639f01dc1393d4933abd2844d1881bf
gonglixue <724351282@qq.com>
2017-12-07 23:36:05 +0800

    simple fps

---
+++ Assets/Scripts/Simple_FPS/Rocket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public float speed = 2000.0f;
+    public GameObject explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);  // destroy the rocket itself; Destroy(this) would only destroy the script attached to the rocket object.
+    }
+}
--- Assets/Scripts/SlotAttackSys/PlayerController.cs
+++ Assets/Scripts/SlotAttackSys/PlayerController.cs
@@ -24,4 +24,6 @@ public class PlayerController : MonoBehaviour {
             }
         }
 	}
+
+
 }
